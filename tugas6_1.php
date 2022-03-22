a53313d5905a495d6dacb30d521f5ef0277c2d56
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2022-03-22 20:06:00 +0700

    Tugas 6 PHP
    Tugas ini saya kirimkan untuk Team Niomic,Terimakasih.

---
+++ tugas6_1.php
@@ -0,0 +1,10 @@
+<?php
+$habib = 21;
+$aji = $habib + 4;
+$bintan = $aji + 3;
+
+echo "====================================================================<br>";
+echo "Usia Habib = $habib <br>";
+echo "Usia Aji = $aji <br>";
+echo "Usia Bintan = $bintan <br>";
+echo "====================================================================";
---
+++ tugas6_2.php
@@ -0,0 +1,10 @@
+<?php
+$jambu = 15000;
+$kardus = 2000;
+$total = (6 * 5 * $jambu) + (2 * $kardus);
+
+echo "====================================================================<br>";
+echo "Harga Jambu = $jambu / Kg <br>";
+echo "Harga Kardus = $kardus / pcs <br>";
+echo "Total Penjualan (Dus dan Jambu) = Rp $total <br>";
+echo "====================================================================";
